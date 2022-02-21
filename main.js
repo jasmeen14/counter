8346b505c4f31914a6bf16cbc6ff6943a451c7cd
jasmeen14 <jasmeen6199@gmail.com>
2022-02-21 15:40:05 +0100

    updated main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 let count = 0;
+
+const value = document.querySelector("#value");
+const btns =document.querySelectorAll(".btn");
+    //console.log(btns);
+btns.forEach(function(btn){
+    //console.log(btn);
+    btn.addEventListener("click",function(e){ //event object
+ const styles = e.currentTarget.classList;
+ if(styles.contains){
+     count--;
+ }
+ else if (styles.contains("increase")){
+     count++;
+ }
+ else{
+     count = 0;
+ }
+ if(count > 0){
+     value.style.color ="green";
+    }
+    if(count < 0){
+        value.style.color ="red";
+    }
+    if(count == 0){
+        value.style.color = "black";
+    }
+ value.textContent = count;
+    });
+});
